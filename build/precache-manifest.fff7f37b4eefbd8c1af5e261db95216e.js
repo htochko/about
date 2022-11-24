ec217b8af0e0eaeb006a43d9e3cd20511a2ba5aa
helen <lentochko@gmail.com>
2022-11-24 15:05:44 +0300

    refresh

---
+++ build/precache-manifest.fff7f37b4eefbd8c1af5e261db95216e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685f113d6e775aeb8fdb73f1fa76bf95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf1f604f25b97ed7b4f",
+    "url": "/static/css/main.75a1b098.chunk.css"
+  },
+  {
+    "revision": "a5a26ff8ce127f14ce0d",
+    "url": "/static/js/2.b3de1959.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b3de1959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf1f604f25b97ed7b4f",
+    "url": "/static/js/main.d5c709d5.chunk.js"
+  },
+  {
+    "revision": "b3819a17e61afca94b7f",
+    "url": "/static/js/runtime-main.b955cec2.js"
+  },
+  {
+    "revision": "2392915b9255581817da5bf3c552dbd7",
+    "url": "/static/media/logo.2392915b.svg"
+  }
+]);
